f2d3ba3ad749ae61209904ed0b964ae2d152ad56
haylachoi <haylachoi@gmail.com>
2025-07-29 18:31:50 +0700

    remove redudant things

--- OpenIddictDemo.Web/obj/Debug/net9.0/OpenIddictDemo.Web.AssemblyInfo.cs
+++ OpenIddictDemo.Web/obj/Debug/net9.0/OpenIddictDemo.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OpenIddictDemo.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+505d4f95411d99be9042383f9c45d58314396cb7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b3bcc951c92e2a223679b72b8cc8ece3ef125f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("OpenIddictDemo.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OpenIddictDemo.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
